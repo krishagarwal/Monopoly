715026911a8b1eda3f954f314240f680182d486a
Sonia Agarwal <agarwal_sonia@hotmail.com>
2016-02-13 19:59:47 -0800

    Added code to Dice class.

---
+++ src/com/agarwal/program.java
@@ -0,0 +1,12 @@
+package com.agarwal;
+
+/**
+ * Created by sagarwal on 2/13/2016.
+ */
+public class program {
+    public static void main(String[] args){
+        int value= Dice.rollDice()  ;
+
+        System.out.println("value: " + value);
+    }
+}

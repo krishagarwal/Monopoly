3fc0f19efac0780bbfa9f5bb95f0434136d13071
Ishika Agarwal <ishikaagarwal@live.com>
2016-02-13 19:23:26 -0800

    made other methods

---
+++ src/CreditCard.java
@@ -0,0 +1,27 @@
+//Ishika
+import java.util.Scanner;
+public class CreditCard {
+    static public double amountOfBalanceInMillion;
+    static private String nameOfPlayer;
+    static Scanner myScan = new Scanner(System.in);
+
+    CreditCard(String nameOfPlayer){
+        amountOfBalanceInMillion = 15;
+    }
+
+    private  static void addMoney(String playerName)
+    {
+        System.out.println("How much should be added to " + playerName + "'s account?");
+        amountOfBalanceInMillion = (myScan.nextDouble()) + amountOfBalanceInMillion;
+    }
+
+    private static boolean subtractMoney(String playerName)
+    {
+        System.out.println("How much money should be subtracted from " + playerName + "'s account?");
+        double amountInMillion = 0.0;
+
+        if(amountOfBalanceInMillion < amountInMillion){
+            System.out.println("Oh no! " + playerName + " doesn't have enough money!")
+        }
+    }
+}

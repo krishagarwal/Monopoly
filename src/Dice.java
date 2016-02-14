4009b85f4d12af405b2146b2c000a8a52d98c0fc
Sonia Agarwal <agarwal_sonia@hotmail.com>
2016-02-13 19:45:15 -0800

    Added code to Dice class.

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,14 @@
 /**
  * Created by Sonia Agarwal on 2/13/2016.
  */
+import java.util.Random;
+
 public class Dice {
+    static Random rand;
+    Dice(){
+        rand=new Random();
+    }
+    public static int rollDice(){
+        return rand.nextInt(12)+2;
+    }
 }

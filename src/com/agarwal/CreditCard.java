c18f18ac47483f4393353266593776fee3c64ba8
Ishika Agarwal <ishikaagarwal@live.com>
2016-02-13 19:44:25 -0800

    krish told me to do something. I don't know, but i dont care... hee hee hee

--- src/com/agarwal/CreditCard.java
+++ src/com/agarwal/CreditCard.java
@@ -6,18 +6,16 @@ public class CreditCard {
     static private String nameOfPlayer;
     static Scanner myScan = new Scanner(System.in);
 
-    CreditCard(String nameOfPlayer){
+    CreditCard(){
         amountOfBalanceInMillion = 15;
     }
 
-    private  static void addMoney()
-    {
+    private  static void addMoney() {
         System.out.println("How much should be added to " + nameOfPlayer + "'s account?");
         amountOfBalanceInMillion = (myScan.nextDouble()) + amountOfBalanceInMillion;
     }
 
-    private static boolean subtractMoney()
-    {
+    private static boolean subtractMoney() {
         System.out.println("How much money should be subtracted from " + nameOfPlayer + "'s account?");
         double amountInMillion = 0.0;
 
